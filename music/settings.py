71e8945c764e116822e097838e9fc47a9b63b60f
chaparod <dachaparro@uc.cl>
2021-05-03 09:46:27 -0400

    pushing to master heroku

--- music/settings.py
+++ music/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    }   #os.path.join(BASE_DIR, 'db.sqlite3')
 }
 
 
@@ -122,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
