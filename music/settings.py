70abf78f403a74101fa39f9fd0ac508185a47ebc
chaparod <dachaparro@uc.cl>
2021-05-03 09:40:34 -0400

    +procfile & requirements.txt

--- music/settings.py
+++ music/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-e4kh6h(pceadv7690gq@871x1ucxprtg6_uv4r1)%k0q6$-e5r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0',
+
+                ]
 
 
 # Application definition
